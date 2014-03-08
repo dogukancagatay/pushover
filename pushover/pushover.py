ac038bf7992017326b86b320472e1fa6fa49baa6
Dogukan Cagatay <dcagatay@gmail.com>
2014-03-08 15:05:52 +0200

    additional message options are added

--- pushover/pushover.py
+++ pushover/pushover.py
@@ -57,27 +57,51 @@ class PushoverClient(object):
             self.conf = { "app_key": self.parser.get("pushover","app_key"),
                  "user_key": self.parser.get("pushover","user_key")}
 
-    def send_message(self, message):
+    def send_message(self, message, title="", url="", url_title="", sound="", priority="", device=""):
         if len(message) > 512:
             raise PushoverMessageTooBig("The supplied message is bigger than 512 characters.")
+        
         payload = {
                 "token": self.conf["app_key"],
                 "user" : self.conf["user_key"],
                 "message": message,
         }
+
+        if title:
+            if len(title) > 100: 
+                payload["title"] = title[:75]
+            else: 
+                payload["title"] = title
+        if url:
+            payload["url"] = url
+        if url_title:
+            payload["url_title"] = url_title
+        if sound:
+            payload["sound"] = sound
+        if priority:
+            payload["priority"] = priority
+        if device:
+            payload["device"] = device
+        
         r = requests.post("https://api.pushover.net/1/messages.json", data=payload )
         if not r.status_code == requests.codes.ok:
             raise r.raise_for_status()
-                      
+
 def main():
     from optparse import OptionParser
     parser = OptionParser()
     parser.description = "This module will send a message through the Pushover.net notification service. It requires at least the '-m' / '--message' parameter to be passed."
     parser.add_option("-c", "--config", dest = "configfile", help = "Location of the Pushover config file.")
-    parser.add_option("-t", "--app-key", dest = "app_key", help = "The app key of the message sender application.")
-    parser.add_option("-u", "--user-key", dest = "user_key", help = "The User/Group key of the message receiver.")
+    parser.add_option("-a", "--app-key", dest = "app_key", help = "The app key of the message sender application.")
+    parser.add_option("-k", "--user-key", dest = "user_key", help = "The User/Group key of the message receiver.")
     parser.add_option("-d", "--debug", dest = "debug", action = "store_true", help = "Log at the DEBUG loglevel.")
     parser.add_option("-m", "--message", dest = "message", help = "The message to send, will truncate to 512 chars.")
+    parser.add_option('-t', '--title', dest = "title", help = 'Message title (max. 100)', default="")
+    parser.add_option('-u', '--url', dest = "url", help = 'Supplementary URL', default="")
+    parser.add_option('-w', '--url-title', dest = "url_title", help = 'Supplementary URL title (max. 100)', default="")
+    parser.add_option('-s', '--sound', dest = "sound", help = 'Notification sound', default="")
+    parser.add_option('-p', '--priority', dest = "priority", help = 'Message priority (-1,0,1)', default="")
+    parser.add_option('-v', '--device', dest = "device", help = 'Message receiver device name', default="")
 
     if len(sys.argv) <= 1:
         parser.print_help()
@@ -87,7 +111,8 @@ def main():
     if options.debug:
         loglevel = logging.DEBUG
     else:
-        loglevel = logging.INFO
+        loglevel = logging.WARNING
+
     logging.basicConfig(level=loglevel, format="%(asctime)s [%(module)s] %(levelname)s: %(message)s")
 
     client = PushoverClient(configfile=options.configfile, app_key=options.app_key, user_key=options.user_key)
@@ -98,7 +123,9 @@ def main():
         parser.error("Can't do anything without a message now can I?")
 
     try:
-        client.send_message(options.message)
+        client.send_message(message=options.message, title=options.title, url=options.url, url_title=options.url_title, sound=options.sound, priority=options.priority, device=options.device)
     except Exception as e:
         logger.critical("Something went wrong: {0}".format(e))
 
+if __name__ == "__main__":
+    main()
