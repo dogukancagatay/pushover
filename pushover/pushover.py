dc34f75749f687a53a5583d61ed724c7a56dc3c2
Dogukan Cagatay <dcagatay@gmail.com>
2014-03-08 13:52:12 +0200

    conf with arguments  added

--- pushover/pushover.py
+++ pushover/pushover.py
@@ -37,15 +37,25 @@ class PushoverMessageTooBig(PushoverException):
 
 class PushoverClient(object):
     """ PushoverClient, used to send messages to the Pushover.net service. """
-    def __init__(self, configfile=""):
+    def __init__(self, configfile="", app_key="", user_key=""):
         self.configfile = configfile
-        self.parser = SafeConfigParser()
-        self.files = self.parser.read([self.configfile, os.path.expanduser("~/.pushover")])
-        if not self.files:
-            logger.critical("No valid configuration found, exiting.")
-            raise PushoverException("No valid configuration found")
-        self.conf = { "app_key": self.parser.get("pushover","app_key"),
-             "user_key": self.parser.get("pushover","user_key")}
+        self.conf = None 
+
+        if app_key and user_key:
+            logger.debug("Using provided app key and user key.")
+            self.conf = { "app_key": app_key, "user_key": user_key }
+        elif not configfile:
+            logger.critical("No configuration provided, exiting.")
+            raise PushoverException("No configuration provided")
+        else:
+            logger.debug("Using app key and user key from configuration file.")
+            self.parser = SafeConfigParser()
+            self.files = self.parser.read([self.configfile, os.path.expanduser("~/.pushover")])
+            if not self.files:
+                logger.critical("No valid configuration found, exiting.")
+                raise PushoverException("No valid configuration found")
+            self.conf = { "app_key": self.parser.get("pushover","app_key"),
+                 "user_key": self.parser.get("pushover","user_key")}
 
     def send_message(self, message):
         if len(message) > 512:
@@ -54,7 +64,7 @@ class PushoverClient(object):
                 "token": self.conf["app_key"],
                 "user" : self.conf["user_key"],
                 "message": message,
-        }   
+        }
         r = requests.post("https://api.pushover.net/1/messages.json", data=payload )
         if not r.status_code == requests.codes.ok:
             raise r.raise_for_status()
@@ -64,8 +74,11 @@ def main():
     parser = OptionParser()
     parser.description = "This module will send a message through the Pushover.net notification service. It requires at least the '-m' / '--message' parameter to be passed."
     parser.add_option("-c", "--config", dest = "configfile", help = "Location of the Pushover config file.")
+    parser.add_option("-t", "--app-key", dest = "app_key", help = "The app key of the message sender application.")
+    parser.add_option("-u", "--user-key", dest = "user_key", help = "The User/Group key of the message receiver.")
     parser.add_option("-d", "--debug", dest = "debug", action = "store_true", help = "Log at the DEBUG loglevel.")
     parser.add_option("-m", "--message", dest = "message", help = "The message to send, will truncate to 512 chars.")
+
     if len(sys.argv) <= 1:
         parser.print_help()
         sys.exit(1)
@@ -77,10 +90,7 @@ def main():
         loglevel = logging.INFO
     logging.basicConfig(level=loglevel, format="%(asctime)s [%(module)s] %(levelname)s: %(message)s")
 
-    if options.configfile:
-        client = PushoverClient(options.configfile)
-    else:
-        client = PushoverClient()
+    client = PushoverClient(configfile=options.configfile, app_key=options.app_key, user_key=options.user_key)
 
     if options.message:
         options.message = options.message[:512]
